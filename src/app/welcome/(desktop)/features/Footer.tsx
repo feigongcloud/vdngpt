6159b17ebe900705b0150b75d0bd7341be3e4d58
shenxiaojian <sjpn741@163.com>
2024-03-18 16:11:18 +0800

    2024-03-18-update-ui

--- src/app/welcome/(desktop)/features/Footer.tsx
+++ src/app/welcome/(desktop)/features/Footer.tsx
@@ -1,24 +1,24 @@
 'use client';
 
-import { ActionIcon, DiscordIcon } from '@lobehub/ui';
+// import { ActionIcon, DiscordIcon } from '@lobehub/ui';
 import { useTheme } from 'antd-style';
-import { Book, Github } from 'lucide-react';
+// import { Book, Github } from 'lucide-react';
 import { memo } from 'react';
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
 
-import { DISCORD, DOCUMENTS, GITHUB } from '@/const/url';
+// import { DISCORD, DOCUMENTS, GITHUB } from '@/const/url';
 
 const Footer = memo(() => {
   const theme = useTheme();
-  const { t } = useTranslation('common');
+  // const { t } = useTranslation('common');
 
   return (
     <Flexbox align={'center'} horizontal justify={'space-between'} style={{ padding: 16 }}>
       <span style={{ color: theme.colorTextDescription }}>
-        ©{new Date().getFullYear()} LobeHub
+        ©{new Date().getFullYear()} VdnCloud
       </span>
-      <Flexbox horizontal>
+      {/* <Flexbox horizontal>
         <ActionIcon
           icon={DiscordIcon}
           onClick={() => window.open(DISCORD, '__blank')}
@@ -37,9 +37,11 @@ const Footer = memo(() => {
           size={'site'}
           title={'GitHub'}
         />
-      </Flexbox>
+      </Flexbox> */}
     </Flexbox>
   );
 });
 
 export default Footer;
+
+
--- src/app/welcome/(desktop)/layout.desktop.tsx
+++ src/app/welcome/(desktop)/layout.desktop.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Logo } from '@lobehub/ui';
+// import { Logo } from '@lobehub/ui';
 import { PropsWithChildren, memo } from 'react';
 import { Center, Flexbox } from 'react-layout-kit';
 
@@ -19,7 +19,7 @@ const Desktop = memo<PropsWithChildren>(({ children }) => {
         horizontal
         style={{ position: 'relative' }}
       >
-        <Logo className={styles.logo} size={36} type={'text'} />
+        {/* <Logo className={styles.logo} size={36} type={'text'} /> */}
         <Flexbox className={styles.view} flex={1}>
           {children}
         </Flexbox>

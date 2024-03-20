57115dfb2339b461aa9557131c905572f3354c65
shenxiaojian <sjpn741@163.com>
2024-03-20 10:23:20 +0800

    2024-03-20-update-ui

--- src/components/FullscreenLoading/index.tsx
+++ src/components/FullscreenLoading/index.tsx
@@ -1,4 +1,4 @@
-import { Icon, Logo } from '@lobehub/ui';
+import { Icon } from '@lobehub/ui';
 import { Loader2 } from 'lucide-react';
 import { memo } from 'react';
 import { Center, Flexbox } from 'react-layout-kit';
@@ -7,7 +7,8 @@ const FullscreenLoading = memo<{ title?: string }>(({ title }) => {
   return (
     <Flexbox height={'100%'} style={{ userSelect: 'none' }} width={'100%'}>
       <Center flex={1} gap={12} width={'100%'}>
-        <Logo extra={'Chat'} size={48} type={'combine'} />
+        {/* <Logo extra={'Chat'} size={48} type={'combine'} /> */}
+  <img src='https://g2.vdnlink.cn/vdn-ai-lunch01.webp'  width={72} />
         <Center gap={16} horizontal>
           <Icon icon={Loader2} spin />
           {title}
